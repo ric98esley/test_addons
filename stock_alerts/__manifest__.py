2d4a3c99776a7fc8c7e0ee69da9c884a177bd2f4
Ricardo Pérez <ricardo.pereze.98@gmail.com>
2025-11-28 06:29:32 -0400

    [ADD] (models views)

--- stock_alerts/__manifest__.py
+++ stock_alerts/__manifest__.py
@@ -6,9 +6,14 @@
     "website": "https://github.com/ric98esley/test_addons",
     "category": "Uncategorized",
     "version": "16.0.0.0.1",
-    "depends": ["base"],
+    "depends": ["base", "stock", "mail"],
     "data": [
-        # 'security/ir.model.access.csv',
+        "security/ir.model.access.csv",
+        "data/ir_cron.xml",
+        "views/product_template_views.xml",
+        "views/stock_alert_history_views.xml",
+        "views/stock_alert_dashboard.xml",
+        "views/menu_items.xml",
     ],
     # only loaded in demonstration mode
     "demo": [
