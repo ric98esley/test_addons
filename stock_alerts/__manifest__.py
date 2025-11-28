e460b85a7704b9b256c305ffe0ba174a34ffc0f4
Ricardo Pérez <ricardo.pereze.98@gmail.com>
2025-11-27 22:37:07 -0400

    [FETCH] init stock alerts

---
+++ stock_alerts/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "stock_alerts",
+    "summary": """
+        Generate alerts when stock levels are low""",
+    "author": "Ricardo Perez",
+    "website": "https://github.com/ric98esley/test_addons",
+    "category": "Uncategorized",
+    "version": "16.0.0.0.1",
+    "depends": ["base"],
+    "data": [
+        # 'security/ir.model.access.csv',
+    ],
+    # only loaded in demonstration mode
+    "demo": [
+        "demo/demo.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "license": "AGPL-3",
+}
